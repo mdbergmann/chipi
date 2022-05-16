0be9731b8c198c28ad1b180bdaba95236c58ce1d
Manfred Bergmann <mb@software-by-mabe.com>
2022-05-16 20:37:50 +0200

    respond :ok

--- src/eta.lisp
+++ src/eta.lisp
@@ -6,4 +6,4 @@
 (in-package :cl-eta.eta)
 
 (defun start-record ()
-  nil)
+  :ok)
---
+++ test/eta-test.lisp
@@ -0,0 +1,19 @@
+(defpackage :cl-eta.eta-test
+  (:use :cl :fiveam :cl-eta.eta)
+  (:export #:run!
+           #:all-tests
+           #:nil))
+(in-package :cl-eta.eta-test)
+
+(def-suite eta-tests
+  :description "ETA tests"
+  :in cl-eta.tests:test-suite)
+
+(in-suite eta-tests)
+
+
+(test start-record--ok
+  (is (eq :ok (start-record)))
+  )
+
+(run! 'start-record--ok)
