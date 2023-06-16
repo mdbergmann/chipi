160b4a84d5b73c071d08008efe00969c761b06f2
Manfred Bergmann <mb@software-by-mabe.com>
2023-06-16 13:45:04 +0200

    minor regfactoring.

--- src/hab.lisp
+++ src/hab.lisp
@@ -20,19 +20,17 @@
 
 (defmacro defitems (&body body)
   (let ((items (gensym "items")))
-    `(progn
-       (setf ,items (list ,@body))
+    `(let ((,items (list ,@body)))
        (setf *items* (alexandria:alist-hash-table ,items)))))
 
 (defmacro item (id label &body body)
   (let ((item (gensym "item"))
         (bindings (gensym "bindn"))
         (binding (gensym "bind")))
-    `(progn
-       (setf ,item (item:make-item ,id ,label))
-       (let ((,bindings (list ,@body)))
-         (dolist (,binding ,bindings)
-           (item:add-binding ,item ,binding)))
+    `(let ((,item (item:make-item ,id ,label))
+           (,bindings (list ,@body)))
+       (dolist (,binding ,bindings)
+         (item:add-binding ,item ,binding))
        (cons ,id ,item))))
 
 (defmacro <fun> (&rest keys)
