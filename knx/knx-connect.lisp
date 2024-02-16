8b8365a6df3aee1bbb0bdf9cf14f892243359b3b
Manfred Bergmann <mb@software-by-mabe.com>
2024-02-16 23:51:14 +0100

    parsing description-response with dib, etc...

--- knx/knx-connect.lisp
+++ knx/knx-connect.lisp
@@ -7,8 +7,22 @@
   (:export #:connect-to-knx
            #:disconnect-from-knx
            #:send-descr-request
+           ;; knx header
            #:knx-header
+           #:header-len
+           #:header-type
+           #:header-knxnetip-version
+           #:header-body-len
+           ;; knx package
+           #:knx-package
+           #:package-header
+           #:package-body
+           ;; descr-response
            #:knx-descr-response
+           #:descr-response-device-hardware
+           #:descr-response-supp-svc-families
+           #:descr-response-other-dev-info
+           #:dib
            ))
 
 (in-package :chipi.knx-connect)
@@ -66,38 +80,87 @@
 
 (defconstant +knx-header-len+ #x06)
 (defconstant +knx-netip-version+ #x10)
+
 (defconstant +knx-descr-request+ #x0203)
 (defconstant +knx-descr-response+ #x0204)
 
-(defstruct (knx-header (:constructor %make-knx-header)
+(defstruct (knx-header (:constructor %make-header)
                        (:conc-name header-))
-  (header-size +knx-header-len+)
+  (len +knx-header-len+)
   (knxnetip-version +knx-netip-version+)
   type
-  payload-len)
+  body-len)
 
-(defstruct knx-package
-  knx-header)
+(defstruct (knx-package (:conc-name package-))
+  (header nil :type (or null knx-header))
+  body)
+
+(defconstant +dib-typecodes-device-info+ #x01)
+(defconstant +dib-typecodes-supp-svc-families+ #x02)
+(defconstant +dib-typecodes-ip-config+ #x03)
+(defconstant +dib-typecodes-ip-cur-config+ #x04)
+(defconstant +dib-typecodes-knx-addresses+ #x05)
+(defconstant +dib-typecodes-mfr-data+ #xfe)
+
+(defstruct (dib (:constructor %make-dib))
+  len type data)
+
+(defun %parse-dibs (body-data)
+  (let ((sub-data body-data)
+        (dibs nil))
+    (loop
+      (when (= 0 (length sub-data))
+        (return))
+      (let* ((len (- (elt sub-data 0) 2))
+             (typecode (elt sub-data 1))
+             (end-index (+ 2 len))
+             (data (subseq sub-data 2 end-index))
+             (dib (%make-dib :len len :type typecode :data data)))
+        (setf sub-data (subseq sub-data end-index))
+        (setf dibs (append dibs (list dib)))))
+    dibs))
 
 (defstruct (knx-descr-response (:include knx-package)
-                               (:constructor %make-knx-descr-response)))
+                               (:constructor %make-descr-response)
+                               (:conc-name descr-response-))
+  (device-hardware (error "Required device-hardware (dip)") :type dib)
+  (supp-svc-families (error "Required supp svc families (dip)") :type dib)
+  (other-dev-info nil))
 
-(defun %parse-knx-package (pkg-data)
+(defun %parse-header (pkg-data)
   (let ((header-size (elt pkg-data 0))
         (knx-version (elt pkg-data 1))
         (type (%to-int (elt pkg-data 2)
                        (elt pkg-data 3)))
-        (payload-size (%to-int (elt pkg-data 4)
-                               (elt pkg-data 5))))
-    (let ((header (%make-knx-header
-                   :header-size header-size
-                   :knxnetip-version knx-version
-                   :type type
-                   :payload-len payload-size)))
-      (cond
-        ((= type +knx-descr-response+)
-         (%make-knx-descr-response
-          :knx-header header))))))
+        (body-size (%to-int (elt pkg-data 4)
+                            (elt pkg-data 5))))
+    (let ((eff-body-size (- body-size +knx-header-len+)))
+      (%make-header
+       :len header-size
+       :knxnetip-version knx-version
+       :type type
+       :body-len eff-body-size))))
+
+(defun %parse-descr-response (header body)
+  (let ((dibs (%parse-dibs body)))
+    (%make-descr-response
+     :header header
+     :body body
+     :device-hardware (first dibs)
+     :supp-svc-families (second dibs)
+     :other-dev-info (nthcdr 2 dibs))))
+
+(defun %parse-knx-package (pkg-data)
+  (let* ((header (%parse-header pkg-data))
+         (header-len (header-len header))
+         (body (subseq pkg-data
+                       header-len
+                       (+ (header-body-len header)
+                          header-len)))
+         (type (header-type header)))
+    (cond
+      ((= type +knx-descr-response+)
+       (%parse-descr-response header body)))))
 
 ;; -----------------------------
 ;; high-level comm
