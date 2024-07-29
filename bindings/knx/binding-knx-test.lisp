0d5b1deb418993be6d9e4378abbb00a1754fe146
Manfred Bergmann <mb@software-by-mabe.com>
2024-07-29 19:16:29 +0200

    binding listens tests OK.

--- bindings/knx/binding-knx-test.lisp
+++ bindings/knx/binding-knx-test.lisp
@@ -1,5 +1,5 @@
 (defpackage :chipi.binding.knx-test
-  (:use :cl :fiveam :chipi.binding.knx)
+  (:use :cl :fiveam :cl-mock :chipi.binding.knx)
   (:export #:run!
            #:all-tests
            #:nil))
@@ -14,19 +14,52 @@
 
 (test make-knx-binding
   "Tests creating a knx binding"
-  (let ((cut (knx-binding
-              :ga "1/2/3"
-              :dpt "9.001")))
-    (is-true cut)
-    (is (address:knx-group-address-p (group-address cut)))
-    (is (eq 'dpt:dpt-9.001 (dpt-type cut)))))
+  (with-mocks ()
+    ;; binding should register listener function
+    (answer knx-client:add-tunnelling-request-listener t)
+    (let ((cut (knx-binding
+                :ga "1/2/3"
+                :dpt "9.001")))
+      (is-true cut)
+      (is (address:knx-group-address-p (group-address cut)))
+      (is (eq 'dpt:dpt-9.001 (dpt-type cut)))
+      (is (= 1 (length (invocations 'knx-client:add-tunnelling-request-listener)))))))
 
-;; (test binding-listens-on-ga-changes
-;;   (let ((cut (knx-binding
-;;               :ga "1/2/3"
-;;               :dpt "9.001")))
-;;     ()
-;;   )
+(defun %make-test-tun-req ()
+  (tunnelling:make-tunnelling-request
+   :channel-id 1
+   :seq-counter 0
+   :cemi (cemi:make-default-cemi
+          :message-code cemi:+cemi-mc-l_data.ind+
+          :dest-address (address:make-group-address "1/2/3")
+          :apci (cemi:make-apci-gv-write)
+          :dpt (dpt:make-dpt1 'dpt:dpt-1.001 :on))))
+
+(test binding-listens-on-ga-changes
+  (with-mocks ()
+    (let ((item-set-called-with nil)
+          (listener-fun-registered nil))
+      ;; binding should register listener function
+      (answer (knx-client:add-tunnelling-request-listener fun)
+        (progn
+          (setf listener-fun-registered fun)
+          t))
+      ;; binding should call `set-value' on bound items
+      (answer (item:set-value _ value)
+        (progn
+          (setf item-set-called-with value)
+          t))
+
+      (let ((cut (knx-binding :ga "1/2/3" :dpt "1.001")))
+        (binding:bind-item cut :foo-item)
+
+        (is (functionp listener-fun-registered))
+        ;; we call the registered fun manually
+        ;; in production this is done automatically
+        (funcall listener-fun-registered (%make-test-tun-req))
+
+        (is (equal 'item:true item-set-called-with))
+        ))))
 
 
 (run! 'knx-binding-tests)
--- bindings/knx/binding-knx.lisp
+++ bindings/knx/binding-knx.lisp
@@ -1,5 +1,5 @@
 (defpackage :chipi.binding.knx
-  (:use :cl :binding :knxc :knx-conn.address :knx-conn.dpt)
+  (:use :cl :binding :knxc :knx-conn.address :knx-conn.dpt :knx-conn.knx-obj)
   (:nicknames :binding-knx)
   (:export #:knx-binding
            #:group-address
@@ -15,10 +15,50 @@
         :initform (error "DPT type required!")
         :reader dpt-type)))
 
+(defun %make-listener-fun (binding ga dpt-type)
+  (lambda (req)
+    (let* ((cemi (tunnelling:tunnelling-request-cemi req))
+           (req-ga (cemi:cemi-destination-addr cemi)))
+      (log:debug "Received request for ga: ~a, required: ~a" req-ga ga)
+      (when (and (equalp req-ga ga)
+                 (eql (tunnelling:tunnelling-cemi-message-code req)
+                      cemi:+cemi-mc-l_data.ind+)
+                 (cemi:apci-gv-write-p (cemi:cemi-apci cemi)))
+        (handler-case
+            (let* ((cemi-data (cemi:cemi-data cemi))
+                   (dpt (etypecase cemi-data
+                          (dpt
+                           cemi-data)
+                          ((vector octet)
+                           (parse-to-dpt dpt-type
+                                         cemi-data))))
+                   (value (dpt:dpt-value dpt)))
+              (log:info "Write value: ~a for ga: ~a"
+                        dpt ga)
+              ;; set item value
+              (let ((converted-value
+                      (cond
+                        ((eq dpt-type 'dpt:dpt-1.001)
+                         (cond
+                           ((eq :on value) 'item:true)
+                           ((eq :off value) 'item:false)))
+                        (t value))))
+                (loop :for item :in (binding::bound-items binding)
+                      :do (progn
+                            (log:debug "Setting on item: ~a" item)
+                            (item:set-value item converted-value)))))
+          (error (e)
+            (log:error "Error in listener-fun: ~a" e)))))))
+
 (defun %make-knx-binding (&key ga dpt)
-  (make-instance 'knx-binding
-                 :ga (make-group-address ga)
-                 :dpt (value-type-string-to-symbol dpt)))
+  (let* ((ga-obj (make-group-address ga))
+         (dpt-type (value-type-string-to-symbol dpt))
+         (binding (make-instance 'knx-binding
+                                 :ga ga-obj
+                                 :dpt dpt-type)))
+    (knx-client:add-tunnelling-request-listener
+     (%make-listener-fun binding ga-obj dpt-type))
+    binding))
 
 (defmacro knx-binding (&rest args)
   `(%make-knx-binding ,@args))
