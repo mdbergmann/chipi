9e2bb4ed182fef38cd5794465154ae3c9fe30a29
Manfred Bergmann <mb@software-by-mabe.com>
2025-07-01 22:50:02 +0200

    Bootstrap 5. Finally switchable switch that toggles a boolean item.

--- src/ui/main.lisp
+++ src/ui/main.lisp
@@ -9,13 +9,15 @@
 (in-package :chipi-ui.main)
 
 (defun on-main (body)
-  (load-css (html-document body) "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css")
-  (load-css (html-document body) "/custom-styles.css")
-  (load-script (html-document body) "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js")
-  (load-script (html-document body) "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js")
+  (load-css (html-document body)
+            "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css")
+  (load-css (html-document body)
+            "/custom-styles.css")
+  (load-script (html-document body)
+               "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js")
 
   (let ((container (create-div body :class "container")))
-    (create-div container :class "main-title" :content "Home Automation Dashboard")
+    (create-div container :class "main-title" :content "Chipi Home Automation Dashboard")
     (let ((itemgroups-container (create-div container :class "itemgroups")))
       (map nil (lambda (ig) (render-itemgroup ig itemgroups-container)) (retrieve-itemgroups)))))
 
@@ -32,19 +34,37 @@
 (defun render-item (item parent)
   (let* ((item-div (create-div parent :class "item"))
          (item-state (gethash "item-state" item))
+         (item-name (gethash "name" item))
          (type-hint (gethash "type-hint" item)))
     
     (create-div item-div :class "item-name"
-                :content (gethash "name" item))
+                         :content item-name)
     
     (create-div item-div :class (format nil "item-type ~a" (string-downcase type-hint))
-                :content (format-type-hint type-hint))
+                         :content (format-type-hint type-hint))
     
-    (create-div item-div :class "item-value"
-                :content (format-value (gethash "value" item-state) type-hint))
-    
+    (render-item-value item-name (gethash "value" item-state) type-hint item-div)
+
     (create-div item-div :class "item-timestamp"
-                :content (format-timestamp (gethash "timestamp" item-state)))))
+                         :content (format-timestamp (gethash "timestamp" item-state)))))
+
+(defun render-item-value (item-name item-value type-hint parent)
+  (cond
+    ((string= "BOOLEAN" type-hint)
+     (let* ((form-check (create-div parent :class "form-check form-switch"))
+            (toggle-input (create-form-element form-check "checkbox"
+                                               :role "switch"
+                                               :class "form-check-input")))
+       (setf (checkedp toggle-input) item-value)
+       (set-on-change toggle-input
+                      (lambda (obj)
+                        (let ((current-state (checkedp obj)))
+                          (format t "Toggled (change): ~a = ~a~%" obj current-state)
+                          (itemsc:update-item-value item-name current-state)
+                          )))))
+    (t
+     (create-div parent :class "item-value"
+                        :content (format-value item-value type-hint)))))
 
 (defun format-timestamp (timestamp)
   (local-time:format-rfc1123-timestring nil (local-time:unix-to-timestamp timestamp)))
@@ -67,6 +87,9 @@
     (t "Undefined type")))
 
 (defun start-main ()
-  (initialize 'on-main
-              :static-root (merge-pathnames "./ui/static-files/")
-              :extended-routing t))
+  (let ((system-root (merge-pathnames "ui/static-files/"
+                                      (asdf:system-source-directory :chipi))))
+    (format t "Root: ~a~%" system-root)
+    (initialize 'on-main
+                :static-root system-root
+                :extended-routing t)))
